ceb78e2b72a635212f5d3043b6adf02c4a66f4b6
G.Ambatte <sean@robertson-family.nz>
2025-10-20 12:59:37 +1300

    Tweak DPCGL text string

--- api/data/dpcgl.js
+++ api/data/dpcgl.js
@@ -1,3 +1,1 @@
-export default `
-This product includes materials from the Daggerheart System Reference Document 1.0, © Critical Role, LLC. under the terms of the Darrington Press Community Gaming (DPCGL) License. More information can be found at https://www.daggerheart.com. There are no previous modifications by others.
-`;
+export default `This product includes materials from the Daggerheart System Reference Document 1.0, © Critical Role, LLC. under the terms of the Darrington Press Community Gaming (DPCGL) License. More information can be found at https://www.daggerheart.com. There are no previous modifications by others.`;
