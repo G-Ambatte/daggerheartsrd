4c0b7db3da58ea7cd92718b07dcc1b4b09a69397
G.Ambatte <sean@robertson-family.nz>
2025-11-09 21:46:04 +1300

    Ignore JSON files during coverage testing

--- vitest.config.js
+++ vitest.config.js
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig, configDefaults } from 'vitest/config';
 
 export default defineConfig({
 	test : {
 		coverage : {
-			enabled : true
+			enabled : true,
+			exclude : [
+				...configDefaults.exclude,
+				'**/*.json'
+			]
 		},
 	},
 });
