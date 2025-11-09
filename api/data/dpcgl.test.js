31dc11600f4ecee14f6c7cf21f6ac37c72aef159
G.Ambatte <sean@robertson-family.nz>
2025-11-09 21:38:06 +1300

    Remove unnecessary DPCGL test

--- api/data/dpcgl.test.js
+++
@@ -1,9 +0,0 @@
-import { describe, test, expect } from 'vitest';
-
-import dpcgl from './dpcgl.js';
-
-describe('DPCGL tests', ()=>{
-	test('Check DPCGL text', async ()=>{
-		expect(dpcgl).toMatchSnapshot();
-	});
-});
