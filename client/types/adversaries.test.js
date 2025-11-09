31dbb4fa6ca837be4fad9c352215af969a37c811
G.Ambatte <sean@robertson-family.nz>
2025-11-09 21:35:00 +1300

    Add alternate test for Adversary output format

--- client/types/adversaries.test.js
+++ client/types/adversaries.test.js
@@ -36,6 +36,47 @@ describe('Check Adversary Type', ()=>{
 			experience  : 'experience',
 			attribution : 'attribution',
 			thresholds  : {
+				major  : 1,
+				severe : 2
+			},
+			attack : {
+				name   : 'attack',
+				range  : 'range',
+				mod    : 'mod',
+				damage : 'damage'
+			},
+			features : [
+				{
+					name        : 'featName',
+					type        : 'featType',
+					description : 'featDescription'
+				}
+			],
+			sources : [
+				{
+					id        : 'sourceID',
+					set       : 'sourceSet',
+					updated   : 'sourceUpdated',
+					publisher : 'sourcePublisher'
+				}
+			]
+		};
+
+		expect(testObj.formatFn(testData)).toMatchSnapshot();
+	});
+
+	test('Format function should return known result for alternate data', async ()=>{
+		const testData = {
+			name        : 'test',
+			tier        : 'tier',
+			type        : 'type',
+			description : 'description',
+			tactics     : 'tactics',
+			difficulty  : 'difficulty',
+			hitPoints   : 'hitPoints',
+			stress      : 'stress',
+			attribution : 'attribution',
+			thresholds  : {
 				major  : 0,
 				severe : 0
 			},
